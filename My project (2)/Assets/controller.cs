20901d3df516abd4d6a3444f54e5eadb158ce4ea
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-03-15 12:18:53 +0100

    modif de jump

--- My project (2)/Assets/controller.cs
+++ My project (2)/Assets/controller.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class controller : MonoBehaviour
 {
-<<<<<<< HEAD
-=======
-    public float speed = 100f; 
-    public float jumpForce = 10f; 
-    public float doubleJumpForce = 12f; 
-    private Rigidbody2D rb; 
-    private bool isGrounded; 
-    private bool doubleJump; 
->>>>>>> 081757e7d8ef8a0bfb30609c67776ee516ac0501
 
     public Rigidbody2D rb2d;
     public SpriteRenderer rend;
     public float Speed = 5;
-    public float JumpSpeed = 50;
+    public float JumpSpeed = 5;
 
     void Start()
     {
@@ -30,15 +21,7 @@ public class controller : MonoBehaviour
         float h = Input.GetAxis("Horizontal") * Speed;
         rb2d.velocity = new Vector2(h, rb2d.velocity.y);
 
-<<<<<<< HEAD
         if (Input.GetKeyDown(KeyCode.Space))
-=======
-        
-        isGrounded = Physics2D.Linecast(transform.position, GameObject.Find("player").transform.position, 1 << LayerMask.NameToLayer("sol"));
-
-        
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
->>>>>>> 081757e7d8ef8a0bfb30609c67776ee516ac0501
         {
             rb2d.AddForce(transform.up * JumpSpeed, ForceMode2D.Impulse);
         }
