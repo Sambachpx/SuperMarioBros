b279abd8bbe7eeb4f705d0449435c1bac4e04b76
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-03-15 07:22:46 +0100

    deplacement player double jump etc (pas encore fonctionel)

--- My project (2)/Assets/controller.cs
+++ My project (2)/Assets/controller.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class controller : MonoBehaviour
+public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f; 
+    public float jumpForce = 10f; 
+    public float doubleJumpForce = 12f; 
+    private Rigidbody2D rb; 
+    private bool isGrounded; 
+    private bool doubleJump; 
+
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector2 position = rb.position + Vector2.right * horizontal * speed * Time.deltaTime;
+        rb.MovePosition(position);
+
+        
+        isGrounded = Physics2D.Linecast(transform.position, GameObject.Find("player").transform.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            doubleJump = true;
+        }
+        
+        else if (Input.GetKeyDown(KeyCode.Space) && doubleJump)
+        {
+            rb.AddForce(Vector2.up * doubleJumpForce, ForceMode2D.Impulse);
+            doubleJump = false;
+        }
     }
 }
